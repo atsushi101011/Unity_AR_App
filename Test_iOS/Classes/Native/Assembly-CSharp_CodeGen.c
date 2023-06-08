9c4cf5ec206b180e817e0e0b81e653934ca83fac
atsushi101011 <atushi10112313@gmail.com>
2023-06-08 10:05:51 +0900

    first commit

---
+++ Test_iOS/Classes/Native/Assembly-CSharp_CodeGen.c
@@ -0,0 +1,53 @@
+﻿#include "pch-c.h"
+#ifndef _MSC_VER
+# include <alloca.h>
+#else
+# include <malloc.h>
+#endif
+
+
+#include "codegen/il2cpp-codegen-metadata.h"
+
+
+
+
+
+// 0x00000001 System.Void PlaceOnPlane::Awake()
+extern void PlaceOnPlane_Awake_m6C1DE9914CD75B0D522422F16A4EEB1269582753 (void);
+// 0x00000002 System.Void PlaceOnPlane::Update()
+extern void PlaceOnPlane_Update_m4BAC7B0437CB4CB61EF9C23849AC096E5EE7C2B7 (void);
+// 0x00000003 System.Void PlaceOnPlane::.ctor()
+extern void PlaceOnPlane__ctor_mEAC8D0276C9BC36A8A633F162E3F8EBF96D6255C (void);
+static Il2CppMethodPointer s_methodPointers[3] = 
+{
+	PlaceOnPlane_Awake_m6C1DE9914CD75B0D522422F16A4EEB1269582753,
+	PlaceOnPlane_Update_m4BAC7B0437CB4CB61EF9C23849AC096E5EE7C2B7,
+	PlaceOnPlane__ctor_mEAC8D0276C9BC36A8A633F162E3F8EBF96D6255C,
+};
+static const int32_t s_InvokerIndices[3] = 
+{
+	2050,
+	2050,
+	2050,
+};
+IL2CPP_EXTERN_C const Il2CppCodeGenModule g_AssemblyU2DCSharp_CodeGenModule;
+const Il2CppCodeGenModule g_AssemblyU2DCSharp_CodeGenModule = 
+{
+	"Assembly-CSharp.dll",
+	3,
+	s_methodPointers,
+	0,
+	NULL,
+	s_InvokerIndices,
+	0,
+	NULL,
+	0,
+	NULL,
+	0,
+	NULL,
+	NULL,
+	NULL, // module initializer,
+	NULL,
+	NULL,
+	NULL,
+};
